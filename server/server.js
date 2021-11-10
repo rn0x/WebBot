64da1fc674b2c8239224fa2ff5303ed1852e9fb2
rn0x <76129163+rn0x@users.noreply.github.com>
2021-11-10 20:46:34 +0300

    update

--- server/server.js
+++ server/server.js
@@ -11,9 +11,13 @@ export default function server(app) {
         const port_http = 80;
         const key = fs.readFileSync('./server/ssl/server.key', 'utf8');
         const cert = fs.readFileSync('./server/ssl/server.crt', 'utf8');
+        const config = fs.readJsonSync('./config.json');
 
         https.createServer({ key, cert }, app) .listen(port_https);
         http.createServer(app).listen(port_http);
+
+        console.log(`Website Link LocalHost https://localhost or http://localhost`)
+        console.log(`Website Link Domain https://${config.domain} or http://${config.domain}`)
 
     } catch(error) {
 
